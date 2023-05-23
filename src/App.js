e52f88e53b80a242563b30692018d1ae3bc071e1
Madjerx <madjerx@hotmail.fr>
2023-05-23 17:28:45 +0200

    add header and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/Header";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import "./header.css";
+import { useLocation } from "react-router-dom";
+import Logo from "../../assets/va_logo.png";
 
 const Header = () => {
+  const location = useLocation();
+  const path = location.pathname;
+  console.log("path = ", path);
   /* Change background Header */
   window.addEventListener("scroll", function () {
     const header = document.querySelector(".header");
@@ -10,74 +15,62 @@ const Header = () => {
   });
   /* Activate menu */
   const [Toggle, showMenu] = useState(false);
-  const [activeNav, setActiveNav] = useState("#home");
 
   return (
     <header className="header">
       <nav className="nav container">
-        <span className="nav__logo">
-          Logo
-        </span>
+        {/* <span className="nav__logo">Logo</span> */}
+        <img className="logo" src={Logo} alt="Vie Animale Logo"/>
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">
               <a
-                href="#home"
-                onClick={() => setActiveNav("#home")}
-                className={
-                  activeNav === "#home" ? "nav__link active-link" : "nav__link"
-                }
+                href="/"
+                className={path === "/" ? "nav__link active-link" : "nav__link"}
               >
                 <i className="uil uil-estate nav__icon"></i> Accueil
               </a>
             </li>
             <li className="nav__item">
               <a
-                href="#about"
-                onClick={() => setActiveNav("#about")}
+                href="/"
                 className={
-                  activeNav === "#about" ? "nav__link active-link" : "nav__link"
+                  path === "/#" ? "nav__link active-link" : "nav__link"
                 }
               >
-                <i className="uil uil-user nav__icon"></i> Le Refuge
+                <i className="uil uil-house-user nav__icon"></i> Le Refuge
               </a>
             </li>
             <li className="nav__item">
               <a
-                href="#skills"
-                onClick={() => setActiveNav("#skills")}
+                href="/"
                 className={
-                  activeNav === "#skills"
-                    ? "nav__link active-link"
-                    : "nav__link"
+                  path === "/#" ? "nav__link active-link" : "nav__link"
                 }
               >
-                <i className="uil uil-file-alt nav__icon"></i> Animaux à adopter
+                <i className="bx bxs-cat bx-tada nav__icon"></i>Nos Animaux
               </a>
             </li>
             <li className="nav__item">
               <a
-                href="#exp"
-                onClick={() => setActiveNav("#exp")}
+                href="/"
                 className={
-                  activeNav === "#exp" ? "nav__link active-link" : "nav__link"
+                  path === "/#" ? "nav__link active-link" : "nav__link"
                 }
               >
-                <i className="uil uil-briefcase-alt nav__icon"></i> Nous aider
+                <i className="bx bx-donate-heart nav__icon"></i> Nous aider
               </a>
             </li>
             <li className="nav__item">
               <a
-                href="#work"
-                onClick={() => setActiveNav("#work")}
+                href="/contact"
                 className={
-                  activeNav === "#work" ? "nav__link active-link" : "nav__link"
+                  path === "/contact" ? "nav__link active-link" : "nav__link"
                 }
               >
-                <i className="uil uil-briefcase-alt nav__icon"></i> Contact
+                <i className="uil uil-chat nav__icon"></i> Contact
               </a>
             </li>
-            
           </ul>
           <i
             className="uil uil-times nav__close"
@@ -85,7 +78,7 @@ const Header = () => {
           ></i>
         </div>
         <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
-          <i className="uil uil-apps"></i>
+          <i className="bx bx-menu-alt-right toggle__mobile__menu"></i>
         </div>
       </nav>
     </header>
