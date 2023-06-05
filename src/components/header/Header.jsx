53738a3f57a3199661c66c3029b7b8daade429ad
Madjerx <madjerx@hotmail.fr>
2023-06-05 11:07:27 +0200

    add sub menu to animaux link

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import "./header.css";
 import { Link, useLocation } from "react-router-dom";
 import Logo from "../../assets/va_logo.png";
+import dogIcon from "../../assets/dog-svgrepo-com.svg";
+import catIcon from "../../assets/cat-svgrepo-com.svg";
+import nacIcon from "../../assets/rabbit-svgrepo-com.svg";
 
 const Header = () => {
   const location = useLocation();
@@ -15,14 +18,16 @@ const Header = () => {
   });
   /* Activate menu */
   const [Toggle, showMenu] = useState(false);
-
+  const [subMenu, toSubMenu] = useState(false);
   return (
     <header className="header">
       <nav className="nav container">
         <span className="nav__logo">Vie Animale</span>
-        {/* <img className="logo" src={Logo} alt="Vie Animale Logo" /> */}
+        {/* <img className="logo" src={Logo} alt="Vie Animale Refuge Logo" /> */}
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
-          <ul className="nav__list grid">
+          <ul
+            className={subMenu ? "nav__list hide_menu grid" : "nav__list grid"}
+          >
             <li className="nav__item">
               <Link
                 to="/"
@@ -42,18 +47,10 @@ const Header = () => {
                 <i className="uil uil-house-user nav__icon"></i> Le Refuge
               </Link>
             </li> */}
-            <li className="nav__item">
-              <Link
-                onClick={() => showMenu(false)}
-                to="/nos-animaux-au-refuge"
-                className={
-                  path === "/nos-animaux-au-refuge"
-                    ? "nav__link active-link"
-                    : "nav__link"
-                }
-              >
+            <li className="nav__item pointer">
+              <div onClick={() => toSubMenu(true)} className="nav__link">
                 <i className="bx bxs-cat bx-tada nav__icon"></i>Nos Animaux
-              </Link>
+              </div>
             </li>
             <li className="nav__item">
               <Link
@@ -80,9 +77,76 @@ const Header = () => {
               </Link>
             </li>
           </ul>
+          <ul
+            className={
+              subMenu ? "sub__nav__list grid" : "sub__nav__list hide_menu grid"
+            }
+          >
+            
+            <li className="nav__item">
+              <Link
+                to="/"
+                onClick={() => {
+                  showMenu(!Toggle);
+                  toSubMenu(!subMenu);
+                }}
+                className={path === "/xx" ? "nav__link active-link" : "nav__link"}
+              >
+                <img src={dogIcon} alt="dog-icon-menu" className="animal_icon" />
+                Les Chiens
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link
+                onClick={() => {
+                  showMenu(!Toggle);
+                  toSubMenu(!subMenu);
+                }}
+                to="/nos-animaux-au-refuge"
+                className={
+                  path === "/xx"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
+                <img src={catIcon} alt="dog-icon-menu" className="animal_icon" />
+                Les Chats
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link
+                onClick={() => {
+                  showMenu(!Toggle);
+                  toSubMenu(!subMenu);
+                }}
+                to="/aider-notre-association"
+                className={
+                  path === "/xx"
+                    ? "nav__link active-link"
+                    : "nav__link"
+                }
+              >
+                <img src={nacIcon} alt="dog-icon-menu" className="animal_icon" />
+                Les NAC
+              </Link>
+            </li>
+            <li className="nav__item back_from_sub_menu pointer">
+              <div
+                onClick={() => {
+                  toSubMenu(!subMenu);
+                }}
+                className="nav__link back_link"
+              >
+                <i class='bx bx-arrow-back'></i><span>Menu Principal</span>
+              </div>
+            </li>
+          </ul>
           <i
             className="uil uil-times nav__close"
-            onClick={() => showMenu(!Toggle)}
+            onClick={() => {
+              showMenu(!Toggle);
+              toSubMenu(!subMenu);
+            }}
           ></i>
         </div>
         <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
